551e95e2bf512fdc42446c9e20fabef1cbcaf5d5
范淼 <femiajayi705@gmail.com>
2021-04-27 16:21:16 +0800

    feat: Added a smutation to create a new user

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,20 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 module.exports = {
-  hello() {
-    return { text: "Hello World", views: 123 };
+  createUser: async ({ userInput }, req) => {
+    const { email, name, password } = userInput;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("User exists already!!!");
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(password, 12);
+    const createdUser = new User({
+      email,
+      name,
+      password: hashedPw,
+    });
+    const user = await createdUser.save();
+    return { ...user._doc, _id: user._id.toString() };
   },
 };
